f71522273f69f6c69b163b755f8a3e4f767ebf83
Alexander Lead <a.lead@yandex.ru>
2023-11-21 22:02:08 +0300

    feat(main): created main window

---
+++ src/GamePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    public GamePanel () {
+        this.setPreferredSize( new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame("Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+//        Add GamePanel to the window
+
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
     }
 }
