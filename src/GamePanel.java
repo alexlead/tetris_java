feaed7f92af8a9859ea59b88cf1124c85e6aebff
Alexander Lead <a.lead@yandex.ru>
2023-11-22 14:46:20 +0300

    feat(play area): added method for drawing play area

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+
+public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameTread;
+    PlayManager pm;
 
     public GamePanel () {
         this.setPreferredSize( new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+
+        pm = new PlayManager();
     }
 
+    public void launchGame () {
+        gameTread = new Thread(this);
+        gameTread.start();
+    }
+
+    @Override
+    public void run() {
+
+//        Game loop
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameTread != null ) {
+            currentTime=System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+
+
+
+    public void update() {
+        pm.update();
+
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        pm.draw(g2);
+    }
 }
